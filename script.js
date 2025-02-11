caa12be253e2e8bcb7333d8a70183cd70c95c06c
Nursingmadesimple <nursingmadesimple101@gmail.com>
2025-02-11 08:53:25 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// Define playlist URLs
+var playlistUrls = {
+    'AnatomyAndPhysiology': 'https://videolectures101.github.io/anatomyandphysiology/',
+    'Pharmacology': 'https://videolectures101.github.io/pharmacology/',
+    'NursingEthics': 'https://videolectures101.github.io/nursingethics/'
+};
+
+// Get DOM elements
+var topicSelector = document.getElementById('topicSelector');
+var goButton = document.getElementById('goButton');
+
+// Enable/disable button based on selection
+topicSelector.addEventListener('change', function () {
+    goButton.disabled = !this.value;
+});
+
+// Navigate to selected playlist
+goButton.addEventListener('click', function () {
+    var selectedTopic = topicSelector.value;
+    if (selectedTopic && playlistUrls[selectedTopic]) {
+        window.open(playlistUrls[selectedTopic], '_blank');
+    }
+});
